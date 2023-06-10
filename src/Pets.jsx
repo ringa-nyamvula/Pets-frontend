626ed70c516ef07bc9c9d78d1660c002dec9499e
ringa-nyamvula <nyamvularinga@gmail.com>
2023-06-10 06:08:53 +0300

    Adding pets function

--- src/Pets.jsx
+++ src/Pets.jsx
@@ -1,51 +1,63 @@
 import React, { useEffect, useState } from "react";
 
-
-
-
-
 function Pets() {
-    const [pets,setPets] = useState([])
-    useEffect(() => {
-        fetch("http://localhost:9292/pets")
-        .then(res => res.json())
-        .then(data => setPets(data))
-    },[])
+  const [pets, setPets] = useState([]);
+  const [newPet, setNewPet] = useState({
+    name: "",
+    breed: "",
+    image_url: ""
+  });
+  const [editPetData, setEditPetData] = useState(null);
 
-    function handleDelete(id) {
-        fetch(`http://localhost:9292/pets/${id}`,{
-            method: "Delete"
-            
-        })
-        .then(res => res.json())
-        .then(data => {
-            let rem = pets.filter(pet => pet.id !== id)
-            setPets(rem)
-        })
-    }
+  useEffect(() => {
+    fetch("http://localhost:9292/pets")
+      .then((res) => res.json())
+      .then((data) => setPets(data));
+  }, []);
 
-    return (
-        <>
-         <ul>
-            {pets.map(pet => (
-                <div className="pet-card" key={pet.id}> 
-                    <li>{pet.name}</li>
-                    <img src={pet.image_url}   alt={pet.name}/>
-                    <p>{pet.breed}</p>
-                    <button onClick={() => handleDelete(pet.id)}>Delete</button>
-                </div>
-            ))}
-         </ul>
-         <h2>Add new pet</h2>
-         <form>
-            <input placeholder="name" />
-            <input placeholder="breed" />
-            <input placeholder="image_url" />
-            <button>Add</button>
-         </form>
+  function handleDelete(id) {
+    fetch(`http://localhost:9292/pets/${id}`, {
+      method: "DELETE"
+    })
+      .then((res) => res.json())
+      .then(() => {
+        setPets(prevPets => prevPets.filter(pet => pet.id !== id));
+        alert("Record deleted!");
+      })
+    //   .catch((error) => {
+    //     console.error("Error deleting pet:", error);
+    //   });
+  }
 
-        </>
-    )
-}
+  function addPet(event) {
+    event.preventDefault();
+    const petData = {
+      name: event.target[0].value,
+      breed: event.target[1].value,
+      image_url: event.target[2].value
+    };
 
-export default Pets
+    fetch("http://localhost:9292/pets", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(petData)
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setPets(prevPets => [...prevPets, data]);
+        alert("Pet added!");
+      })
+      .catch((error) => {
+        console.error("Error adding pet:", error);
+      });
+
+    event.target.reset();
+  }
+
+  function editPet(pet) {
+    setEditPetData(pet);
+  }
+
+  
